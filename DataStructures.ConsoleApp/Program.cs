9b132dd7fa7445c73e6a09c1a0babd696f9e2c65
leojgrant <leo_grant@btinternet.com>
2024-01-27 20:34:57 +0000

    Added a HashMap structure

---
+++ DataStructures.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructures.Service;
+
+Console.Write("Creating an empty hash map with a size of 3 (in all arrays)...");
+HashMap dictionary = new HashMap(3);
+Console.WriteLine("Created!");
+dictionary.DisplayHashMap();
+
+Console.WriteLine();
+Console.WriteLine("Adding values...");
+dictionary.AddKeyValuePair("apple", "This ");
+dictionary.AddKeyValuePair("banana", "actually ");
+dictionary.AddKeyValuePair("orange", "works!");
+Console.WriteLine();
+
+Console.Write("The Hash Map now looks like this: ");
+dictionary.DisplayHashMap();
+
+Console.WriteLine("\nSelecting each value (from left to right you get):\n");
+Console.Write(dictionary.GetValue("apple"));
+Console.Write(dictionary.GetValue("banana"));
+Console.WriteLine(dictionary.GetValue("orange"));
---
+++ DataStructures.Service/HashMap.cs
@@ -0,0 +1,86 @@
+﻿namespace DataStructures.Service;
+
+// This class represents the functionality of HashMap object.
+// HashMaps typically use arrays (of fixed length) not lists (of variable lengths) to store values.
+// The Hash function is used to map a key to an index in this array.
+// If more than one key is mapped ot the same index, this is known as 'Collision'.
+// To avoid Collision you can implement a technique called 'Chaining'.
+// Chaining is when the key-value pairs are stored in the array, not just the values.
+// i.e. [[['apple', 3]], [['banana', 1], ['orange', 4]]] where banana and orange are hashed to the same index.
+public class HashMap
+{
+    private int Size { get; set; }
+
+    private object[,,] HashMapList { get; set; }
+
+    private int[] IndexPositionsForChaining { get; set;}
+    public HashMap(int size)
+    {
+        this.Size = size;
+        this.HashMapList = new object[this.Size, this.Size, 2];
+        this.IndexPositionsForChaining = new int[this.Size];
+    }
+
+    private int HashFunction(object key)
+    {
+        return Hash(key) % this.Size;
+    }
+
+    // NOTE: This is not an example of a hash function, I just implemented this as a mocked example.
+    private int Hash(object key)
+    {
+        if (key == "apple")
+        {
+            return 0;
+        }
+        else if (key == "banana")
+        {
+            return 1;
+        }
+        else { return 1; }
+    }
+
+    public void AddKeyValuePair(object key, object value)
+    {   
+        int index = this.HashFunction(key);
+
+        if (index >= this.Size)
+        {
+            throw new Exception("Unable to add key value pair - memory has reached maximum capacity due to collision.");
+        }
+
+        this.HashMapList[index, IndexPositionsForChaining[index], 0] = key;
+        this.HashMapList[index, IndexPositionsForChaining[index], 1] = value;
+        this.IndexPositionsForChaining[index] += 1;
+    }
+
+    public object GetValue(object key)
+    {
+        int index = this.HashFunction(key);
+
+        for (int i = 0; i < this.Size; i++)
+        {
+            if (this.HashMapList[index, i, 0] == key)
+            {
+                return this.HashMapList[index, i, 1];
+            };
+        }
+
+        throw new Exception("Key is not contained within the HashMap.");
+    }
+
+    public void DisplayHashMap()
+    {
+        Console.Write("\n[");
+        for (int i = 0; i < this.Size;i++)
+        {
+            Console.Write("[");
+            for (int j = 0; j < this.Size; j++)
+            {
+                Console.Write($"[{this.HashMapList[i, j, 0]}, {this.HashMapList[i, j, 1]}]");
+            };
+            Console.Write("]");
+        };
+        Console.Write("]\n");
+    }
+}
